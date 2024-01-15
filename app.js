ad553b36328fda866cd0af1c145c884206e31dd6
Ignacio A. Padilla <nacho20002008@gmail.com>
2024-01-15 20:23:25 -0300

    agregando formularios register y login de practica integradora - Formularios

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Informar que queremos hacer uso de algunos archivos estáticos como recurso.
 const publicPath = path.join(__dirname, './public');
 app.use( express.static(publicPath) );
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.post('/', (req, res) => {
+    res.redirect("/login");
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.post('/login', (req, res) => {
+    res.redirect("/");
 });
